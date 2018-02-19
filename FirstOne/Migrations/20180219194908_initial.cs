c888316afcf6756760aa2a42d4397ca7d5a2ad0a
Tomasz Mól <kkemot123@gmail.com>
2018-02-19 22:19:33 +0100

    DropDownMenu with Edit client problem

---
+++ FirstOne/Migrations/20180219194908_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FirstOne.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ClientID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Program = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ClientID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Client");
+        }
+    }
+}
---
+++ FirstOne/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FirstOne.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Surname { get; set; }
+
+        public String City { get; set; }
+
+        public String Program { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem> Programs { get; set; }
+
+    }
+}
